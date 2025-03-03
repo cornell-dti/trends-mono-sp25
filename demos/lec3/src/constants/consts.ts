e16a0071b0f33fe782b0e6586b05f9d748731d34
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2025-03-02 21:40:47 -0500

    initial demo

---
+++ demos/lec3/src/constants/consts.ts
@@ -0,0 +1,27 @@
+export const COURSES: Course[] = [
+    {
+        subject: "CS",
+        catalogNbr: 1110,
+        titleShort: "Intro Computing: Design & Dev",
+    },
+    {
+        subject: "CS",
+        catalogNbr: 2110,
+        titleShort: "Obj-Oriented Prog & Data Struc",
+    },
+    {
+        subject: "CS",
+        catalogNbr: 2800,
+        titleShort: "Math Foundations of CS",
+    },
+    {
+        subject: "MATH",
+        catalogNbr: 1920,
+        titleShort: "Multivariable Calculus Engrs",
+    },
+    {
+        subject: "PHYS",
+        catalogNbr: 1112,
+        titleShort: "Physics I: Mechanics & Heat",
+    },
+];
