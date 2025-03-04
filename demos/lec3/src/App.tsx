5b26683d47302065649221d9108f9d96b87de66c
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2025-03-03 19:13:51 -0500

    lec 3 demo update (#2)
    * dropdown
    
    * more boilerplate
    
    * new consts

--- demos/lec3/src/App.tsx
+++ demos/lec3/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import CoursePlan from "./components/CoursePlan";
 
 const App = () => {
   return (
     <div>
       <h1>CoursePlan Clone</h1>
+      <CoursePlan />
     </div>
   );
 };
