10732a748852c3b9ae911f8f46be3128fa4f4261
Oscar Wang <oscarwang22396@gmail.com>
2025-03-24 16:34:59 -0400

    vite stuff

--- demos/lec6/client/src/vite-env.d.ts
+++ demos/lec6/client/src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="vite/client" />
-/// <reference types="vite/types/importMeta.d.ts" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPER_SECRET_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
