700437bea06941632e1237e2feae82ad45ed365a
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2025-04-14 20:52:09 -0400

    finish lec8 (#6)
    * finish lec8
    
    * rm firebaseconfig

--- demos/lec8/client/src/firebase.ts
+++ demos/lec8/client/src/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Our web app's Firebase configuration (example)
 const firebaseConfig = {
-  apiKey: "AIzaSyDh8P3MP012iezI7moLr9IooWyRvBmFDVg",
-  authDomain: "trends-sp25-lec8-test.firebaseapp.com",
-  projectId: "trends-sp25-lec8-test",
-  storageBucket: "trends-sp25-lec8-test.firebasestorage.app",
-  messagingSenderId: "14252358306",
-  appId: "1:14252358306:web:92de6f8a2977d0cac9b5fb"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
